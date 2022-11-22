0d4a8bba06ee023cf0b8a8b07c5f435aeb7d19cf
aechristopoulos <aec47459@usc.edu>
2022-11-22 14:33:25 -0800

    parsed sequence ID, sequence, and sequence length from .fasta file

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import csv 
+from Bio import SeqIO
+
 file = open("sequences\A_Victoria_361_2011_H3N2_NP.fasta", "r")
 # print(file.read())
 
-import csv 
+
 
 with open("parsed_fasta_file.csv", "w") as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+
+
+for seq_record in SeqIO.parse("sequences\A_Victoria_361_2011_H3N2_NP.fasta", "fasta"):
+    print(seq_record.id)
+    print(repr(seq_record.seq))
+    print(len(seq_record))
+
 
