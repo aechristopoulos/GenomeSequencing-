da18b7c450c98f6393345216b981566be02ae97b
aechristopoulos <aec47459@usc.edu>
2022-11-22 15:20:26 -0800

    create my_function

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import csv 
 from Bio import SeqIO
+import os
 
-file = open("sequences\A_Victoria_361_2011_H3N2_NP.fasta", "r")
-# print(file.read())
+# Get the list of all files and directories
+path = ".\sequences"
+dir_list = os.listdir(path)
+ 
+
+def my_function(filename):
+    for seq_record in SeqIO.parse("sequences\A_Victoria_361_2011_H3N2_NP.fasta", "fasta"):
+        sequence_id = seq_record.id
+        sequence = repr(seq_record.seq)
+        sequence_length = len(seq_record)
+    print(filename + "Lexie" )
+
+for i in dir_list:
+    my_function(i)
 
 
 
-with open("parsed_fasta_file.csv", "w") as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-
-
-
-for seq_record in SeqIO.parse("sequences\A_Victoria_361_2011_H3N2_NP.fasta", "fasta"):
-    print(seq_record.id)
-    print(repr(seq_record.seq))
-    print(len(seq_record))
-
-
+# with open("parsed_fasta_file.csv", "w") as csvfile:
+#     filewriter = csv.writer(csvfile, delimiter=',',
+#                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
+#     filewriter.writerow(['sequence ID', sequence_id])
+#     filewriter.writerow(['sequence', sequence])
+#     filewriter.writerow(['sequence length', sequence_length])
