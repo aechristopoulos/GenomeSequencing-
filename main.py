bded08aadee0ad30153c30eff967af32f66f8b05
aechristopoulos <aec47459@usc.edu>
2022-11-22 14:28:30 -0800

    create parsed_fasta_file.csv

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 file = open("sequences\A_Victoria_361_2011_H3N2_NP.fasta", "r")
-print(file.read())
+# print(file.read())
+
+import csv 
+
+with open("parsed_fasta_file.csv", "w") as csvfile:
+    filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
