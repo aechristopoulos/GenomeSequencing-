324b720979a30e95ed0226314c0d6e573df391e7
aechristopoulos <aec47459@usc.edu>
2022-11-22 16:36:16 -0800

    Define write to csv function

--- main.py
+++ main.py
@@ -8,23 +8,26 @@ dir_list = os.listdir(path)
  
 
 def my_function(filename):
-    for seq_record in SeqIO.parse("sequences\A_Victoria_361_2011_H3N2_NP.fasta", "fasta"):
+    for seq_record in SeqIO.parse("sequences/" + filename, "fasta"):
         sequence_id = seq_record.id
         sequence = repr(seq_record.seq)
         sequence_length = len(seq_record)
     print(filename)
+    write_to_csv(sequence_id, sequence, sequence_length)
+
+def write_to_csv(sequence_id, sequence, sequence_length):
+    with open("parsed_file_2.csv", "w+") as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',',
+                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow(['sequence ID', sequence_id])
+        filewriter.writerow(['sequence', sequence])
+        filewriter.writerow(['sequence length', sequence_length])
     print(sequence_id)
     print(sequence)
     print(sequence_length)
+    
+
 
 for i in dir_list:
     my_function(i)
 
-
-
-# with open("parsed_fasta_file.csv", "w") as csvfile:
-#     filewriter = csv.writer(csvfile, delimiter=',',
-#                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#     filewriter.writerow(['sequence ID', sequence_id])
-#     filewriter.writerow(['sequence', sequence])
-#     filewriter.writerow(['sequence length', sequence_length])
