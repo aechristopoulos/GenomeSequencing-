034e7b83080a2a91defd9742b03e029d9073bcc7
aechristopoulos <aec47459@usc.edu>
2022-11-29 10:48:18 -0800

    created rows, header, and cleaned up csv file generation

--- main.py
+++ main.py
@@ -8,36 +8,33 @@ def main():
     path = "./sequences"
     dir_list = os.listdir(path)
 
+    data = []
+    headers = ["Sequence ID", "Sequence", "Sequence Length"]
+
     if os.path.exists("parsed_file.csv"):
         os.remove("parsed_file.csv")
     
     for i in dir_list:
-        my_function(i)
- 
+        my_function(i, data)
 
-def my_function(filename):
+    write_to_csv(headers, data)
+
+def my_function(filename,rows):
     for seq_record in SeqIO.parse("sequences/" + filename, "fasta"):
         sequence_id = seq_record.id
         sequence = repr(seq_record.seq)
         sequence_length = len(seq_record)
-    print(filename)
-    write_to_csv(sequence_id, sequence, sequence_length)
+    rows.append([sequence_id, sequence, sequence_length])
 
-def write_to_csv(sequence_id, sequence, sequence_length):
+
+def write_to_csv(header, data):
     with open("parsed_file.csv", "a") as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
                              quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow(['sequence ID', sequence_id])
-        filewriter.writerow(['sequence', sequence])
-        filewriter.writerow(['sequence length', sequence_length])
-    print(sequence_id)
-    print(sequence)
-    print(sequence_length)
-
-
-
-
-
+        filewriter.writerow(header)
+        filewriter.writerows(data)
+       
+   
 
 
 if __name__ == "__main__":
