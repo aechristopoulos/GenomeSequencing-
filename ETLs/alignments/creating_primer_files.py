25555c08a867b7f0414226b73e7a066eff7bf8fe
aechristopoulos <aec47459@usc.edu>
2023-03-29 16:12:41 -0700

    new NCR primer files

--- ETLs/alignments/creating_primer_files.py
+++ ETLs/alignments/creating_primer_files.py
@@ -22,9 +22,12 @@ def reverse_primers(primer_sequence, id, description):
 # forward_primers("GCAGGTCAATTATATTCAATATGGA", "PB2_Begining_1.1", "Sanger Sequencing begining primer 1, noncoding to position 6")
 # forward_primers("ATTCAATATGGAAAGAATAAAAGAGC", "PB2_Begining_1.2", "Sanger Sequencing begining primer 2, noncoding to position 19")
 # forward_primers("GCAACCAGGAGATTGATTC", "PB2_Middle_1.1", "Sanger Sequencing middle primer, position 1141 to 1166")
+forward_primers("AGCAAAAGCAGGTCAAATATATTCA", "PB2_Forward_1.2", "PB2 foward universal NCR primer, 5' tail not included here")
+
 # #reverse
 # reverse_primers("GTAGAAACAAGGTCGTTTTTAAACTATTC", "PB2_Reverse_2.3", "PB2 reverse primer")
 # reverse_primers("CGAATAATTTAAAAACGACCTTGAA", "PB2_End_1.1", "Sanger Sequencing end primer, position 2297 into noncoding region")
+reverse_primers("AGTAGAAACAAGGTCGTTTTTAAACTATTC", "PB2_Reverse_2.4", "PB2 reverse universal NCR primer, 5' tail not included here")
 
 
 # #PB1 primers:
@@ -32,17 +35,24 @@ def reverse_primers(primer_sequence, id, description):
 # forward_primers("AGCAAAAGCAGGCAAACCATTT", "PB1_Forward_1.2", "PB1 forward primer")
 # forward_primers("TCAATCCGACATTACTTTTCTTAA", "PB1_Begining_1.1", "PB1 Sanger Sequencing begining primer, positions 1 through 33")
 # forward_primers("GAAACTGAGAACTCAAATACCTG", "PB1_Middle_1.1", "PB1 Sanger Sequencing middle primer, positions 1084 to 1111")
+forward_primers("AGCAAAAGCAGGCAAACCATTTG", "PB1_Forward_1.3", "PB1 forward universal NCR primer")
+
 # #reverse
 # reverse_primers("AGAAACAAGGCATTTTTTCATGAAGGA", "PB1_Reverse_2.3", "PB1 reverse primer")
 # reverse_primers("CATGAAGGACAAGCCAAATTC", "PB1_End-1.1", "PB1 Sanger Sequencing end prime 1, positions 2273 to 2300")
 # reverse_primers("ATGAAGGACAAGCCAAATTC", "PB1_End_1.2", "PB1 Sanger Sequencing end primer 2, positions 2273 to 2299")
-
+reverse_primers("AGTAGAAACAAGGCATTTTTTCATGAAGA", "PB1_Reverse_2.4", "PB1 reverse universal NCR primer, 5' tail not included here")
 
 # #PA primers:
 # # forward
 # forward_primers("GCAAAAGCAGGTACTGATYCAAA", "PA_Forward_1.4", "PA forward primer, mixed base")
+forward_primers("AGCAAAAGCAGGTACTGATCCAAAAT", "PA_Forward_1.5", "PA forward NCR primer for H1, H7, and H9 strains")
+forward_primers("AGCAAAAGCAGGTACTGATTCAAAATG", "PA_Forward_1.6", "PA forward NCR primer for H3 and H5 strains")
+forward_primers("AGCAAAAGCAGGTACTGATYCAAAATG", "PA_Forward_1.7", "PA forward universal NCR primer, mixed base")
+
 # # reverse
 # reverse_primers("GGACAGTATGGATAGCAAATAGTAGCA", "PA_Reverse_2.4", "PA reverse primer")
+reverse_primers("AGTAGAAACAGGTACTTTTTTGGACA", "PA_Reverse_2.7", "PA reverse universal NCR primer, 5' tail not included here")
 
 
 # #HA primers: 
@@ -51,19 +61,28 @@ def reverse_primers(primer_sequence, id, description):
 # forward_primers("CAAAAGCAGGGGGAAAACAAAAGAA", "HA_Forward_1.7", "HA forward primer 2")
 # forward_primers("AGCAAAAGCAGGGGATAATAAAAACAAC", "HA_Forward_1.8_Brisbane", "HA forward primer, Brisbane 2007 sequence specific")
 # forward_primers("GGAAAACAAAAGCAACAAAAATGAAGGC", "HA_Forward_1.9", "HA forward primer 3 H1 specific")
-# forard_primers("AGGGGATAATTCTATTAACCATGAAGACT", "HA_Forward_1.10", "HA forward primer 4 H3 specific")
+# forward_primers("AGGGGATAATTCTATTAACCATGAAGACT", "HA_Forward_1.10", "HA forward primer 4 H3 specific")
+forward_primers("AGCAAAAGCAGGGGAAAACAAA", "HA_Forward_1.11", "HA forward NCR primer for H1 strains, 5' tail not included here")
+forward_primers("AGCAAAAGCAGGGGATAATTCTAT", "HA_Forward_1.12", "HA Forward NCR primer for H3 strains, 5' tail not included here")
+
 # # reverse
 # reverse_primers("CAAGGGTGTTTTTCTCATGATTCTGAA", "HA_Reverse_2.3", "HA reverse primer")
 # reverse_primers("GAAACAAGGGTGTTTTTCCTTATATTTCTG", "HA_Reverse_2.4_Brisbane", "HA reverse primer, Brisbane 2007 sequence specific")
 # reverse_primers("CTACATTGTAGAGACCCATTAGAGCAC", "HA_Reverse_2.5", "HA revrrse primer 2 H1 specific")
 # reverse_primers("AATGTTGCATCTAATGTTGCCCTTTTG", "HA_Reverse_2.6", "HA reverse primer 3 H3 specific")
-
+reverse_primers("AGTAGAAACAAGGGTGTTTTT", "HA_Reverse_2.7", "HA reverse universal NCR primer, 5' tail not included here")
 
 # #NP primers: 
 # # forward
 # forward_primers("GCAGGGTWGATAATCACTCA", "NP_Forward_1.5", "NP forward primer, mixed base")
+forward_primers("AGCAAAAGCAGGGTAGATAATCACTCA", "NP_Forward_1.6", "NP forward universal NCR primer for strains H1, H5, H7, and H9")
+forward_primers("AGCAAAAGCAGGGTWGATAATCACTCA", "NP_Forward_1.7", "NP forward universal NCR primer, mixed base")
+forward_primers("AGCAAAAGCAGGGTTGATAATCACTCA", "NP_Forward_1.8", "NP forward NCR primer for H3 strains")
+
 # # reverse
 # reverse_primers("GATCAGTAGAAACAAGGGTATTTTTT", "NP_Reverse_2.3", "NP reverse primer")
+reverse_primers("AGTAGAAACAAGGGTATTTTTCTT", "NP_Reverse_2.5", "NP reverse universal NCR primer, 5' tail not included here")
+
 
 
 # #NA primers: 
@@ -71,19 +90,26 @@ def reverse_primers(primer_sequence, id, description):
 # forward_primers("ATGAATCCAAASCAAAAGATAATAACV", "NA_Forward_1.2", "NA internal forward primer, mixed base")
 # # reverse
 # reverse_primers("CGCATATTAGTAGAAACAAGGAGTT", "NA_Reverse_2.1", "NA enhanced reverse primer, includes part of noncoding region and enchanced 5' prime tail")
-
+reverse_primers("AGTAGAAACAAGGAGTT", "NA_Reverse_2.2", "NA reverse universal NCR primer, 5' tail not included here")
 
 # #MP primers: 
 # # forward 
 # forward_primers("AGCAGCAAAAGCAGGTAGATATTGAAA", "MP_Forward_1.1", "MP forward primer")
+forward_primers("AGCAAAAGCAGGTAGATGTTTAAAGATGAG", "MP_Forward_1.4", "MP forward NCR primer for strains H1, H7, and H9")
+forward_primers("AGCAAAAGCAGGTAGATATTGAAAGATGAG", "MP_Forward_1.5", "MP forward NCR primer for strains H3 and H5")
+forward_primers("AGCAAAAGCAGGTAGATRTTKAAAGATGAG", "MP_Forward_1.6", "MP forward universal NCR primer, mixed base")
 # # reverse
 # reverse_primers("AGACAGGATCAGTAGAAACAAGGTAGTT", "MP_Reverse_2.1", "MP reverse primer")
+reverse_primers("AGTAGAAACAAGGTTAGTTTTTTACT", "MP_Reverse_2.2", "MP reverse universal NCR primer, 5' tail not included here")
 
 
 # #NS primers: 
 # # forward
 # forward_primers("GCAAAAGCAGGGTGACAAA", "NS_Forward_1.2", "NS forward primer")
 # forward_primers("ATGGATTCCCACACTGTGT", "NS_Forward_1.3_Brisbane", "NS forward primer, Brisbane 2007 sequence specific")
+forward_primers("AGCAAAAGCAGGGTGACAAAA", "NS_Forward_1.4", "NS forward universal NCR primer, 5' tail not included here")
 # # reverse
 # reverse_primers("GATCAGTAGAAACAAGGGTGTT", "NS_Reverse_2.1", "NS reverse primer")
+reverse_primers("AGTAGAAACAAGGGTGTT", "NS_Reverse_2.2", "NS reverse universal NCR primer, 5' tail not included here")
+
 
